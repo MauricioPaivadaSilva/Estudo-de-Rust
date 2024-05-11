c04730221739d757c7237e51bf848e088c420e39
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-05-11 15:10:14 -0300

    Assistida a aula 7 novamente.

--- aula_7/src/main.rs
+++ aula_7/src/main.rs
@@ -1,7 +1,27 @@
-fn say_hallo(){
-    println!("\n\tHello\n");
+/*fn say_hallo(name: &str){
+    println!("\n\tHello {name}\n");
 }
 
 fn main(){
-    say_hallo();
+    say_hallo("Maurício");
+}*/
+
+//
+//  Convertendo números e somando usando funções
+//
+
+fn convert_to_number(s: &str) -> i32 {
+    s.parse().unwrap()
+}
+
+fn double(n: i32) -> i32 {
+    n*2
+}
+
+fn main() {
+    let input = "56 65 58 48 59 56 87 23";
+
+    let result: Vec<i32> = input.split(' ').map(convert_to_number).map(double).collect();
+
+    println!("\n\tResultado = {:?}", result);
 }
