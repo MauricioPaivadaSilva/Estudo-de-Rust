cdfbb4e04c4ae48d12e2126e70396e777ecb950a
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-05-11 18:06:45 -0300

    Adicionada a função de soma.

--- Desafio_Pessoal_1/src/main.rs
+++ Desafio_Pessoal_1/src/main.rs
@@ -3,18 +3,26 @@ use std::io;
 fn main() {
     println!("\n{:=^40}", "Calculadora");
 
-    println!("\nDigite os valores que deseja operar (separados pelo tipo de operação): ");
+    println!("\nDigite os valores que deseja operar (separados pelo tipo de operação e por espaço): ");
 
     let mut valores = String::new();
     io::stdin()
         .read_line(&mut valores)
         .expect("\n\tErro na incerção dos valores\n\t");
 
-    if '+' in valores {
-        println!("\n\t Encontrado +\n\t")
-    } else{
-        println!("\n\tDeu ruim\n\t")
+    let nums: Vec<&str> = valores.split(" ").map(|n| n.trim()).collect();
+
+    let mut local = 0;
+    let mut soma = 0;
+
+    for n in nums.clone().into_iter() {
+        if n == "+" {
+            soma = nums[local-1].parse::<i32>().unwrap() + nums[local+1].parse::<i32>().unwrap();
+        } else {
+            println!("\n\tAinda não planejado");
+        }
+        local += 1;
     }
 
-    println!("{:?}", nums);
+    println!("{}", soma);
 }
